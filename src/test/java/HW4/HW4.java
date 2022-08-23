c98b83c79e0f631a1dfd5885438d892a511e6e45
= <=>
2022-08-23 18:01:55 +0300

    CW4 and HW4

---
+++ src/test/java/HW4/HW4.java
@@ -0,0 +1,59 @@
+package HW4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class HW4 {
+    WebDriver driver;
+    WebElement field;
+    WebElement searchButton;
+    WebElement link;
+
+    @BeforeTest
+    public void preconditions(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+    @BeforeMethod
+    public void before(){
+        driver.get("https://www.google.by/");
+    }
+
+    @Test
+    public void googleTest1(){
+        //Системная переменная web driver
+        //System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        //WebDriver driver = new ChromeDriver();
+        //Размер открываемого окна
+        //driver.manage().window().maximize();
+        //Открытие страницы по адресу
+        //driver.get("https://www.google.by/");
+        //Поиск элемента по имени
+        field = driver.findElement(By.name("q"));
+        //Клик по элементу
+        field.click();
+        //Ввод значения в элемент(поле)
+        field.sendKeys("Hello, World!");
+        //Поиск элемента по xpath
+        searchButton = driver.findElement(By.cssSelector("center:nth-child(1) > .gNO89b"));
+        //Клик по кнопке(searchButton)
+        searchButton.click();
+        link = driver.findElement(By.cssSelector("#rso > .MjjYud:nth-child(1) .LC20lb"));
+    }
+    @Test
+    public void googleTest2(){
+        field = driver.findElement(By.name("q"));
+        field.clear();
+        field.click();
+        field.sendKeys("///***///");
+        searchButton = driver.findElement(By.cssSelector("center:nth-child(1) > .gNO89b"));
+        searchButton.click();
+        link = driver.findElement(By.name("q"));
+    }
+}
---
+++ src/test/java/Lecture4/Lecture4_2.java
@@ -0,0 +1,82 @@
+package Lecture4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Lecture4_2 {
+    //System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void preconditions() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+    }
+
+    @Test
+    public void test1() {
+        WebElement zipCode = driver.findElement(By.name("zip_code"));
+        WebElement continueButton = driver.findElement(By.xpath("//*[@value='Continue']"));
+        zipCode.sendKeys("1111");
+        continueButton.click();
+        pause(2);
+        WebElement updatedZipCode = driver.findElement(By.name("zip_code"));
+        Assert.assertTrue(updatedZipCode.isDisplayed());
+    }
+
+    @Test
+    public void test2() {
+        WebElement zipCode = driver.findElement(By.name("zip_code"));
+        WebElement continueButton = driver.findElement(By.xpath("//*[@value='Continue']"));
+        zipCode.clear();
+        zipCode.sendKeys("11111");
+        continueButton.click();
+        pause(2);
+        WebElement updatedZipCode = driver.findElement(By.name("zip_code"));
+        Assert.assertFalse(updatedZipCode.isDisplayed());
+    }
+
+    @Test
+    public void test3() {
+        WebElement zipCode = driver.findElement(By.name("zip_code"));
+        WebElement continueButton = driver.findElement(By.xpath("//*[@value='Continue']"));
+        zipCode.clear();
+        zipCode.sendKeys("111111");
+        continueButton.click();
+        pause(2);
+        WebElement updatedZipCode = driver.findElement(By.name("zip_code"));
+        Assert.assertTrue(updatedZipCode.isDisplayed());
+    }
+
+    @AfterTest
+    public void postconditions(){
+        driver.close();
+    }
+
+    private void pause(Integer timeout) {
+        try {
+            Thread.sleep(timeout * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
